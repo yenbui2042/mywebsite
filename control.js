2fe4962ff5f62f5d059de9fe95ee2ebc91d2161b
yenbui2042 <141967492+yenbui2042@users.noreply.github.com>
2023-08-11 05:52:50 +0700

    Add files via upload

---
+++ control.js
@@ -0,0 +1,49 @@
+// JavaScript Document
+$('#sp-men').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('#new').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('#sp-women').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
